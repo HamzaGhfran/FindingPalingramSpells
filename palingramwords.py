29f294a2283fadd0feac256fc2b743cebc444d17
hamza <hamzaghafran56@gmail.com>
2023-07-30 11:36:58 +0500

    code to find palingram words in dictionary

--- palingramwords.py
+++ palingramwords.py
@@ -2,15 +2,18 @@
 
 import loaddictionary as ld
 
-word_list = ["abbe","ebb"]
-palindrom_list = []
-for word in word_list:
-    end = len(word)
-    revers_word = word[::-1]
-    if end > 1:
-        for i in range(end):
-            if word[i:] == revers_word[:end-i] and revers_word[end-i:] in word_list:
-                palindrom_list.append((word, revers_word[:end-i]))
-            if word[:i] == revers_word[end-i:] and revers_word[:end-i] in word_list:
-                palindrom_list.append((revers_word[:end-i],word))
-print(palindrom_list)
+word_list = ld.load("worddictionary1.txt")
+def palingram():
+    palindrom_list = []
+    for word in word_list:
+        end = len(word)
+        revers_word = word[::-1]
+        if end > 1:
+            for i in range(end):
+                if word[i:] == revers_word[:end-i] and revers_word[end-i:] in word_list:
+                    palindrom_list.append((word, revers_word[:end-i]))
+                if word[:i] == revers_word[end-i:] and revers_word[:end-i] in word_list:
+                    palindrom_list.append((revers_word[:end-i],word))
+    return palindrom_list
+palindrom = palingram()
+print(palindrom)
