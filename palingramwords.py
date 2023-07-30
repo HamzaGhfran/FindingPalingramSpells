673b21ffbe6efd55220543f49ef5f79353ffab74
hamza <hamzaghafran56@gmail.com>
2023-07-30 13:10:51 +0500

    word_list change into set because set is more faster then list

--- palingramwords.py
+++ palingramwords.py
@@ -4,15 +4,16 @@ import loaddictionary as ld
 
 word_list = ld.load("worddictionary1.txt")
 def palingram():
+    words = set(word_list)
     palindrom_list = []
     for word in word_list:
         end = len(word)
         revers_word = word[::-1]
         if end > 1:
             for i in range(end):
-                if word[i:] == revers_word[:end-i] and revers_word[end-i:] in word_list:
+                if word[i:] == revers_word[:end-i] and revers_word[end-i:] in words:
                     palindrom_list.append((word, revers_word[:end-i]))
-                if word[:i] == revers_word[end-i:] and revers_word[:end-i] in word_list:
+                if word[:i] == revers_word[end-i:] and revers_word[:end-i] in words:
                     palindrom_list.append((revers_word[:end-i],word))
     return palindrom_list
 palindrom = palingram()
